45b6c76a6fd2eb3f7a9512a70be8b0d68916c4c0
Juliane Speck <57350721+juliane99@users.noreply.github.com>
2021-02-02 14:25:46 +0100

    added form
    created form with mat form field
    styled css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { BlogOverviewComponent } from './blog-overview/blog-overview.component';
 import { BlogDetailComponent } from './blog-detail/blog-detail.component';
 import { BlogCreateComponent } from './blog-create/blog-create.component';
 import { StartseiteComponent } from './startseite/startseite.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -22,7 +26,13 @@ import { StartseiteComponent } from './startseite/startseite.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
