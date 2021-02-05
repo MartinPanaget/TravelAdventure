04cb87a306dbc1f84ae6506dcbcfab500aa749f9
Juliane Speck <57350721+juliane99@users.noreply.github.com>
2021-02-05 17:13:30 +0100

    added function to back button to go to previous page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common'
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private location: Location) { }
+
+  back(): void {
+    this.location.back()
+  }
 
   ngOnInit(): void {
   }
